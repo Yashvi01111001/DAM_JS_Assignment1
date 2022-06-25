c251d93681090bf9c6b1c12d311717fd6478f37c
Yashvi01111001 <75441982+Yashvi01111001@users.noreply.github.com>
2022-06-25 14:01:23 +0300

    first-commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").onclick = function(){
+
+    var name = document.getElementById("name").value;
+   
+    document.getElementById("txt").innerHTML = "Hello " + name + " ! " + " Have a great day";
+
+}
